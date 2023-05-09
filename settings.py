3e207e8884f0726a15a638f8cfea7c89aa4bc3d3
Brandon <91694942+CPScout22@users.noreply.github.com>
2023-05-09 13:23:22 -0600

    Update settings.py

---
+++ settings.py
@@ -0,0 +1,18 @@
+import pygame as py
+LEVEL_ONE = [
+'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    ',#1
+'x                                                         ',#2
+'x                   x                                     ',#3
+'x                   x                                     ',#4
+'x                   x                                     ',#5
+'x    e              x                                     ',#6
+'xxxxxxxxxxxxx       x                                     ',#7
+'x                x  x                                     ',#8
+'xp                 xx                                     ',#9
+'xxxxxx   xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx    ']#10
+#1234567890123456789012345679
+start = False
+tile_size = 64
+SCREEN_WIDTH = 1280
+SCREEN_HIEGHT = len(LEVEL_ONE * tile_size)
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HIEGHT))
